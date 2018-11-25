655dfb33d19c5b57921c57e876e4f72f14d08156
Chjun-chi Chiu <chjunchi.chiu@gmail.com>
2018-11-25 21:47:55 +0100

    Day1 update

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-var blackjack_1 = require("./blackjack");
-var newGame = new blackjack_1.Game();
-newGame.createDeck();
+var express_1 = __importDefault(require("express"));
+var Server = (function () {
+    function Server() {
+        this.app = express_1.default();
+    }
+    Server.prototype.sendIndex = function () {
+        this.app.get('/', function (req, res) {
+            res.sendFile(__dirname + '/index.html');
+        });
+        this.app.use(express_1.default.static(__dirname + '/public'));
+    };
+    Server.prototype.startServer = function () {
+        this.sendIndex();
+        this.app.listen(3000, function () {
+        });
+    };
+    return Server;
+}());
+var server = new Server();
+server.startServer();
