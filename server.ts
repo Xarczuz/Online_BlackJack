634e3b377e08af7c9903f75db7c2787d88485c93
Chjun-chi Chiu <chjunchi.chiu@gmail.com>
2018-11-25 21:47:55 +0100

    Day1 update

--- server.ts
+++ server.ts
@@ -1,3 +1,42 @@
 import {Game } from "./blackjack";
-let newGame = new Game();
-newGame.createDeck();
+// let newGame = new Game();
+// newGame.createDeck();
+
+
+
+import express from 'express';
+
+// import io from 'socket.io';
+
+
+class Server {
+
+    public app: express.Application;
+    /**
+     * Constructor.
+     *
+     * @class Server
+     * @constructor
+     */
+    constructor() {
+      //create expressjs application
+      this.app = express();
+    }
+    sendIndex(){
+        this.app.get('/', function (req, res) {
+            res.sendFile(__dirname + '/index.html');
+          });
+        this.app.use(express.static(__dirname + '/public'));
+    }
+    startServer(){
+        this.sendIndex();
+        this.app.listen(3000,function(){
+        });
+    }
+
+  }
+
+
+  let server = new Server();
+
+  server.startServer();
